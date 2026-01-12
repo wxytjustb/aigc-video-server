a08e51bf64ee5cdd74dcee4f8036acf8bf6eac4b
rokywang <wxytjustb@gmail.com>
2026-01-12 20:12:19 +0800

    init project

---
+++ web/src/view/dashboard/components/table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="ranking" label="排名" width="80" align="center" />
+      <el-table-column prop="message" label="更新内容" show-overflow-tooltip />
+      <el-table-column prop="author" label="提交人" width="140" />
+      <el-table-column prop="date" label="时间" width="180" />
+    </el-table>
+  </div>
+</template>
+
+<script setup>
+  import { Commits } from '@/api/github'
+  import { formatTimeToStr } from '@/utils/date'
+  import { ref, onMounted } from 'vue'
+
+  const tableData = ref([])
+
+  const loadCommits = async () => {
+    const { data } = await Commits(1)
+    tableData.value = data.slice(0, 5).map((item, index) => {
+      return {
+        ranking: index + 1,
+        message: item.commit.message,
+        author: item.commit.author.name,
+        date: formatTimeToStr(item.commit.author.date, 'yyyy-MM-dd hh:mm:ss')
+      }
+    })
+  }
+
+  onMounted(() => {
+    loadCommits()
+  })
+</script>
+
+<style scoped lang="scss"></style>
