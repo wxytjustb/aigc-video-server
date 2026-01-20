bbbff84a95cb8b6f5478299e73b816428be38587
cwz <15628923278@163.com>
2026-01-20 18:09:03 +0800

    增加casdoor配置

--- server/config/casdoor.go
+++ server/config/casdoor.go
@@ -2,10 +2,9 @@ package config
 
 // Casdoor 登录配置
 type Casdoor struct {
-	Endpoint     string `mapstructure:"endpoint" json:"endpoint" yaml:"endpoint"`
-	ClientID     string `mapstructure:"client-id" json:"clientId" yaml:"client-id"`
-	ClientSecret string `mapstructure:"client-secret" json:"clientSecret" yaml:"client-secret"`
-	//RedirectURL      string `mapstructure:"redirect-url" json:"redirectUrl" yaml:"redirect-url"`
+	Endpoint         string `mapstructure:"endpoint" json:"endpoint" yaml:"endpoint"`
+	ClientID         string `mapstructure:"client-id" json:"clientId" yaml:"client-id"`
+	ClientSecret     string `mapstructure:"client-secret" json:"clientSecret" yaml:"client-secret"`
 	Certificate      string `mapstructure:"certificate" json:"certificate" yaml:"certificate"`
 	OrganizationName string `mapstructure:"organization-name" json:"organizationName" yaml:"organization-name"`
 	ApplicationName  string `mapstructure:"application-name" json:"applicationName" yaml:"application-name"`
